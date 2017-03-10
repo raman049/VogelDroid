82fd95ccc8af1c3a3714d388f61898f7587c71c4
raman049 <raman049@yahoo.com>
2017-03-09 18:28:20 -0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,19 +1,18 @@
 package com.vogelplay.vogel;
 
-import android.app.ActionBar;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.graphics.Typeface;
-import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,14 +20,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         //setContentView(R.layout.activity_main);
-        // Creating a new RelativeLayout
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        // Creating a new FrameLayout
         FrameLayout frameLayout = new FrameLayout(this);
-        // Defining the RelativeLayout layout parameters.
-        // In this case I want to fill its parent
-        FrameLayout.LayoutParams flp = new FrameLayout.LayoutParams(
-                FrameLayout.LayoutParams.MATCH_PARENT ,
-                FrameLayout.LayoutParams.MATCH_PARENT);
         frameLayout.setBackgroundColor(Color.RED);
         //Remove title bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -37,30 +31,74 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // Setting the RelativeLayout as our content view
         setContentView(frameLayout);
+        int width = this.getResources().getDisplayMetrics().widthPixels;
+        int height = this.getResources().getDisplayMetrics().heightPixels;
+        FrameLayout.LayoutParams flp = new FrameLayout.LayoutParams(
+                1000,320);
+        flp.setMargins(width/2 - 500,height/5,0,0);
         // Creating a new TextView
         TextView tv = new TextView(this);
         tv.setText("VOGEL");
+        tv.setGravity(Gravity.CENTER);
         tv.setTextColor(Color.BLUE);
-        tv.setPadding(flp.width/2, flp.height/8, 500, 150);
         tv.setTextSize(100);
-        //Typeface face= Typeface.createFromAsset(getAssets(), "serif");
-        //tv.setTypeface(face);
-        // Defining the layout parameters of the TextView
-        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(
-                FrameLayout.LayoutParams.WRAP_CONTENT,
-                FrameLayout.LayoutParams.WRAP_CONTENT);
-        // Setting the parameters on the TextView
-        //tv.setLayoutParams(lp);
-
-        // Adding the TextView to the RelativeLayout as a child
+        tv.setLayoutParams(flp);
         frameLayout.addView(tv);
 
-        ImageButton playButton = new ImageButton();
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(150, 150);
+        lp.setMargins(width/2 - 75,height/2,0,0);
+        ImageButton playButton = new ImageButton(this);
+        playButton.setLayoutParams(lp);
+        playButton.setBackgroundResource(R.drawable.play);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(MainActivity.this, "You clicked button " + v.getId(), Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+        frameLayout.addView(playButton);
 
+        TextView highScore = new TextView(this);
+        FrameLayout.LayoutParams lpHS = new FrameLayout.LayoutParams(700, 150);
+        lpHS.setMargins(width/2 - 350,height - 150 ,0,0);
+        highScore.setText("HIGH SCORE");
 
+        highScore.setTypeface(Typeface.create("wichita", Typeface.NORMAL));;
+        highScore.setGravity(Gravity.CENTER);
+        highScore.setTextColor(Color.YELLOW);
+        highScore.setTextSize(25);
+        highScore.setLayoutParams(lpHS);
+        frameLayout.addView(highScore);
 
+        ImageButton instruction = new ImageButton(this);
+        FrameLayout.LayoutParams lpInst = new FrameLayout.LayoutParams(150, 150);
+        lpInst.setMargins(10,height-170,0,0);
+        instruction.setLayoutParams(lpInst);
+        instruction.setBackgroundResource(R.drawable.instruction_que);
+        instruction.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(MainActivity.this, "You clicked button " + v.getId(), Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+        frameLayout.addView(instruction);
 
-
+        ImageButton scoreboard = new ImageButton(this);
+        FrameLayout.LayoutParams lpscoreb = new FrameLayout.LayoutParams(150, 150);
+        lpscoreb.setMargins(170,height-170,0,0);
+        scoreboard.setLayoutParams(lpscoreb);
+        scoreboard.setBackgroundResource(R.drawable.scoreboard);
+        scoreboard.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(MainActivity.this, "You clicked button " + v.getId(), Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+        frameLayout.addView(scoreboard);
 
     }
+
 }
