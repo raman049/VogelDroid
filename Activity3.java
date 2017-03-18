fec198747648bc5c85251f207a57deea0c2d2203
raman049 <raman049@yahoo.com>
2017-03-17 17:43:30 -0700

    Update Activity3.java

--- Activity3.java
+++ Activity3.java
@@ -1,82 +1,147 @@
-package com.vogelplay.vogel;
+package com.vogelplay.vogel3;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * Created by maharjan on 3/11/17.
- */
+
 
 public class Activity3 extends AppCompatActivity {
 
-    Rect bird;
-    int canvas_width;
-    int canvas_height;
-    int bird_x;
-    int bird_y;
+    PopupWindow popUpWindow;
+    ImageButton scoreboard;
+    FrameLayout frameLayout3;
 
-    //View view;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        // Creating a new FrameLayout
+        frameLayout3 = new FrameLayout(this);
+        frameLayout3.setBackgroundColor(Color.GRAY);
+//        //Remove title bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+       getSupportActionBar().hide();
+       this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
-        setContentView(new MyView(this));
+//        // Setting the RelativeLayout as our content view
+        setContentView(frameLayout3);
+        int width = this.getResources().getDisplayMetrics().widthPixels;
+        int height = this.getResources().getDisplayMetrics().heightPixels;
+        FrameLayout.LayoutParams flp = new FrameLayout.LayoutParams(
+                1000, 320);
+        flp.setMargins(width / 2 - 500, height / 6, 0, 0);
+//        // Creating a new TextView
+        TextView tv = new TextView(this);
+        tv.setText("VOGEL");
+        tv.setTypeface(Typeface.create("sans-serif-thin", Typeface.NORMAL));
+        tv.setGravity(Gravity.CENTER);
+        tv.setTextColor(Color.BLUE);
+        tv.setTextSize(100);
+       tv.setLayoutParams(flp);
+        frameLayout3.addView(tv);
+
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(150, 150);
+        lp.setMargins(width / 2 - 75, height / 2, 0, 0);
+        ImageButton playButton = new ImageButton(this);
+        playButton.setLayoutParams(lp);
+        playButton.setBackgroundResource(R.drawable.play);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Connect();
+                Intent i = new Intent(com.vogelplay.vogel3.Activity3.this, Activity2.class);
+                startActivity(i);
+                //Toast toast = Toast.makeText(MainActivity.this, "You clicked button " + v.getId(), Toast.LENGTH_LONG);
+                // toast.show();
+            }
+        });
+        frameLayout3.addView(playButton);
+
+        TextView highScore = new TextView(this);
+        FrameLayout.LayoutParams lpHS = new FrameLayout.LayoutParams(700, 150);
+        lpHS.setMargins(width / 2 - 350, height - 200, 0, 0);
+        highScore.setText("HIGH SCORE");
+
+        highScore.setTypeface(Typeface.create("Comic Sans MS", Typeface.NORMAL));
+        highScore.setGravity(Gravity.CENTER);
+        highScore.setTextColor(Color.YELLOW);
+        highScore.setTextSize(25);
+        highScore.setLayoutParams(lpHS);
+        frameLayout3.addView(highScore);
+
+        final ImageButton instruction = new ImageButton(this);
+        popUpWindow = new PopupWindow(this);
+        FrameLayout.LayoutParams lpInst = new FrameLayout.LayoutParams(150, 150);
+        lpInst.setMargins(10, height - 170, 0, 0);
+        instruction.setLayoutParams(lpInst);
+        instruction.setBackgroundResource(R.drawable.instructionque);
+        frameLayout3.addView(instruction);
+        instruction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                instruction.invalidate();
+                scoreboard.invalidate();
+                popUpWindow.showAtLocation(frameLayout3, Gravity.CENTER, 0, 0);
+                final String ss = "skdjf";
+                Log.d(ss, "onClick");
+            }
+        });
+        ImageView instView = new ImageView(com.vogelplay.vogel3.Activity3.this);
+        FrameLayout.LayoutParams lpinstView = new FrameLayout.LayoutParams(width - 100, height - 100);
+        lpinstView.setMargins(0, 0, 0, 0);
+        instView.setLayoutParams(lpinstView);
+        instView.setImageResource(R.drawable.instruction2);
+        FrameLayout popupFrame = new FrameLayout(this);
+        popupFrame.addView(instView);
+
+        ImageButton backButton = new ImageButton(this);
+        FrameLayout.LayoutParams lpbackButton = new FrameLayout.LayoutParams(150, 160);
+        lpbackButton.setMargins(10, 10, 0, 0);
+        backButton.setLayoutParams(lpbackButton);
+        backButton.setBackgroundResource(R.drawable.back);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                instruction.isActivated();
+                scoreboard.isActivated();
+                popUpWindow.dismiss();
+            }
+        });
+        popupFrame.addView(backButton);
+        popUpWindow.setContentView(popupFrame);
+
+
+        scoreboard = new ImageButton(this);
+        FrameLayout.LayoutParams lpscoreb = new FrameLayout.LayoutParams(150, 150);
+        lpscoreb.setMargins(170, height - 170, 0, 0);
+        scoreboard.setLayoutParams(lpscoreb);
+        scoreboard.setBackgroundResource(R.drawable.scoreboard);
+        scoreboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(com.vogelplay.vogel3.Activity3.this, "You clicked button " + v.getId(), Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+        frameLayout3.addView(scoreboard);
 
     }
 
-    public class MyView extends View {
-        public MyView(Context context) {
-            super(context);
-            // TODO Auto-generated constructor stub
-        }
 
-        @Override
-        protected void onDraw(Canvas canvas) {
-            // TODO Auto-generated method stub
-            super.onDraw(canvas);
-            canvas_width = getWidth();
-            canvas_height = getHeight();
-            Paint paint = new Paint();
-            paint.setStyle(Paint.Style.FILL);
-            paint.setColor(Color.argb(255,153,204,255));  //background color
-            canvas.drawRect(0,0,getWidth(),getHeight(),paint);
-
-
-            Bitmap image1 ;
-            bird_x = canvas_width/3;
-            bird_y = canvas_height/2;
-            image1 = BitmapFactory.decodeResource(getResources(),R.drawable.wave2);
-            image1 = Bitmap.createScaledBitmap(image1,image1.getWidth()/3,image1.getHeight()/3,true);
-            canvas.drawBitmap(image1,300,600,null);
-
-            Bitmap birdImage;
-            birdImage = BitmapFactory.decodeResource(getResources(),R.drawable.bird1);
-            birdImage = Bitmap.createScaledBitmap(birdImage,birdImage.getWidth()/3,birdImage.getHeight()/3,true);
-            canvas.drawBitmap(birdImage,bird_x,bird_y,null);
-
-        }
-        @Override
-        public boolean onTouchEvent(MotionEvent event) {
-
-            bird_x = bird_x + 5;
-            bird_y = bird_y + 5;
-            return super.onTouchEvent(event);
-        }
-    }
 }
