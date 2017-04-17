a8ea7b6ed0b9e999f2628c0670e1d3444d75957f
raman049 <raman049@yahoo.com>
2017-04-16 17:37:14 -0700

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -2,11 +2,14 @@ package com.vogelplay.vogel3;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.media.MediaPlayer;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -21,18 +24,24 @@ import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.ErrorDialogFragment;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.wearable.Wearable;
+
+public class MainActivity extends AppCompatActivity  {
 
     PopupWindow popUpWindow;
     ImageButton scoreboard;
     FrameLayout frameLayout;
     MediaPlayer loop1;
+    GoogleApiClient client;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        //setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        //Remove title bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -45,8 +54,8 @@ public class MainActivity extends AppCompatActivity {
         int width = this.getResources().getDisplayMetrics().widthPixels;
         int height = this.getResources().getDisplayMetrics().heightPixels;
         FrameLayout.LayoutParams flp = new FrameLayout.LayoutParams(
-                width/2,height/3);
-        flp.setMargins(width/4,height/6,0,0);
+                width*2/3,WindowManager.LayoutParams.WRAP_CONTENT);
+        flp.setMargins(width/6,height/7,0,0);
  // TEXT_VIEW VOGEL
         TextView tv = new TextView(this);
         tv.setText("VOGEL");
@@ -54,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         tv.setTypeface(face);
         tv.setGravity(Gravity.CENTER);
         tv.setTextColor(Color.BLUE);
-        tv.setTextSize(width/20);
+        tv.setTextSize(height/10);
         tv.setLayoutParams(flp);
         frameLayout.addView(tv);
 
@@ -63,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
         loop1.setLooping(true);
         loop1.start();
 //PLAY BUTTON
-        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(width/10, height/6);
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(width/10, width/10);
         lp.setMargins(width*9/20,height/2,0,0);
         ImageButton playButton = new ImageButton(this);
         playButton.setLayoutParams(lp);
@@ -71,7 +80,6 @@ public class MainActivity extends AppCompatActivity {
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loop1.stop();
                 Intent i = new Intent(MainActivity.this,Activity2.class);
                 startActivity(i);
             }
@@ -79,8 +87,8 @@ public class MainActivity extends AppCompatActivity {
         frameLayout.addView(playButton);
  // HIGHSCORE TEXT
         TextView highScore = new TextView(this);
-        FrameLayout.LayoutParams lpHS = new FrameLayout.LayoutParams(width/2, height/3);
-        lpHS.setMargins(width/4 ,height*3/4 ,0,0);
+        FrameLayout.LayoutParams lpHS = new FrameLayout.LayoutParams(width/2, WindowManager.LayoutParams.WRAP_CONTENT);
+        lpHS.setMargins(width/4 ,height*6/7 -30 ,0,0);
         SharedPreferences prefs = this.getSharedPreferences("myPrefsKey", Context.MODE_PRIVATE);
         int highScore_String;
         if (prefs.getBoolean("myPrefsKey",true)){
@@ -89,17 +97,17 @@ public class MainActivity extends AppCompatActivity {
             prefs.edit().putInt("score", 0).commit();
             highScore_String = prefs.getInt("score", 0);
         }
-        highScore.setText("HIGH SCORE: \n"+highScore_String);
+        highScore.setText("HIGH SCORE: "+highScore_String);
         highScore.setTypeface(face);
         highScore.setGravity(Gravity.CENTER);
         highScore.setTextColor(Color.YELLOW);
-        highScore.setTextSize(height/50);
+        highScore.setTextSize(height/40);
         highScore.setLayoutParams(lpHS);
         frameLayout.addView(highScore);
 
         final ImageButton instruction = new ImageButton(this);
         popUpWindow = new PopupWindow(this);
-        FrameLayout.LayoutParams lpInst = new FrameLayout.LayoutParams(width/10, height/6);
+        FrameLayout.LayoutParams lpInst = new FrameLayout.LayoutParams(width/10, width/10);
         lpInst.setMargins(10,height*6/7 -30,0,0);
         instruction.setLayoutParams(lpInst);
         instruction.setBackgroundResource(R.drawable.instructionque);
@@ -123,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
         popupFrame.addView(instView);
 
         ImageButton backButton = new ImageButton(this);
-        FrameLayout.LayoutParams lpbackButton = new FrameLayout.LayoutParams(width/10, height/6);
+        FrameLayout.LayoutParams lpbackButton = new FrameLayout.LayoutParams(width/10, width/10);
         lpbackButton.setMargins(10,10,0,0);
         backButton.setLayoutParams(lpbackButton);
         backButton.setBackgroundResource(R.drawable.close);
@@ -137,14 +145,17 @@ public class MainActivity extends AppCompatActivity {
         });
         popupFrame.addView(backButton);
         popUpWindow.setContentView(popupFrame);
+
         scoreboard = new ImageButton(this);
-        FrameLayout.LayoutParams lpscoreb = new FrameLayout.LayoutParams(width/10, height/6);
+        FrameLayout.LayoutParams lpscoreb = new FrameLayout.LayoutParams(width/10, width/10);
         lpscoreb.setMargins(width/10 +20,height*6/7 -30,0,0);
         scoreboard.setLayoutParams(lpscoreb);
         scoreboard.setBackgroundResource(R.drawable.scoreboard);
         scoreboard.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+
+
                 Toast toast = Toast.makeText(MainActivity.this, "You clicked button " + v.getId(), Toast.LENGTH_LONG);
                 toast.show();
             }
@@ -153,13 +164,56 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+//    protected void buildGoogleApiClient() {
+//        client = new GoogleApiClient.Builder(this)
+//                .addConnectionCallbacks(this)
+//                .addOnConnectionFailedListener(this)
+//                .addApi(Drive.API)
+//                .addScope(Drive.SCOPE_FILE)
+//                .build();
+//    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        startActivity(intent);
+    }
+
+
     @Override
     protected void onPause() {
         super.onPause();
+        loop1.stop();
+        loop1.release();
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    protected void onRestart() {
+        super.onRestart();
+        loop1 = MediaPlayer.create(this,R.raw.intro1);
+        loop1.setLooping(true);
+        loop1.start();
     }
+
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//    }
+//
+//    @Override
+//    public void onConnected(@Nullable Bundle bundle) {
+//        Log.d("raman","maharjan");
+//    }
+//
+//    @Override
+//    public void onConnectionSuspended(int i) {
+//        Log.d("ramans", "maharjans");
+//    }
+//    @Override
+//    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+//        Log.d("ramanf", "maharjanf");
+//        }
+
 }
